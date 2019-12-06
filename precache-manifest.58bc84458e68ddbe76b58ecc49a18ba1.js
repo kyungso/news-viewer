2898e4850640d31b3abc38606ec5588661328073
kyungso <cks678928@gmail.com>
2019-12-06 18:14:12 +0900

    Updates

---
+++ precache-manifest.58bc84458e68ddbe76b58ecc49a18ba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c4770f29f54e29d9b1d5e34cc8cdcc",
+    "url": "/news-viewer/index.html"
+  },
+  {
+    "revision": "180159decfdaaf04c68e",
+    "url": "/news-viewer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "084ac33c89b5571074a4",
+    "url": "/news-viewer/static/js/2.dfea44a2.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/news-viewer/static/js/2.dfea44a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "180159decfdaaf04c68e",
+    "url": "/news-viewer/static/js/main.65bfd401.chunk.js"
+  },
+  {
+    "revision": "e8ae1e9ca74840555cad",
+    "url": "/news-viewer/static/js/runtime-main.51420b8f.js"
+  }
+]);
